7148e82d8b1867d5f3895c1edff79608892bdab8
Ivan Iziumov <iiziumov@anaconda.com>
2026-01-09 12:11:07 +0100

    mupdf 1.27.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,25 +2,33 @@
 set -ex
 
 # build system uses non-standard env vars
-uname=$(uname)
 if [[ "$target_platform" == osx* ]]; then
-  #export LIBS="${LIBS} -L${PREFIX}/lib -v"
-  #export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -v"
   export CFLAGS="${CFLAGS} -I ${PREFIX}/include/freetype2"
   export CFLAGS="${CFLAGS} -I $(ls -d ${PREFIX}/include/openjpeg-*)"
-  #export SYS_FREETYPE_LIBS=" -lfreetype"
-  #export SYS_FREETYPE_CFLAGS="${CFLAGS}"
+  export CFLAGS="${CFLAGS} -Wno-incompatible-function-pointer-types"
 fi
+
 export CFLAGS="${CFLAGS} -I ${PREFIX}/include/harfbuzz"
 export XCFLAGS="${CFLAGS}"
 export XLIBS="${LIBS}"
+export USE_GUMBO=yes
+export USE_SYSTEM_GUMBO=yes
+export SYS_GUMBO_CFLAGS="-I${PREFIX}/include"
+export SYS_GUMBO_LIBS="-L${PREFIX}/lib -lgumbo"
 export USE_SYSTEM_LIBS=yes
+export USE_SYSTEM_MUJS=yes
 export USE_SYSTEM_JPEGXR=yes
-
-# diagnostics
-#ls -lh ${PREFIX}/lib
+export VENV_FLAG=""
 
 # build and install
-make prefix="${PREFIX}" -j ${CPU_COUNT} all
-# no make check
-make prefix="${PREFIX}" install
+# Clang chokes on large font lexing so reduce the number of jobs
+if [[ "$target_platform" == osx* ]]; then
+  make prefix="${PREFIX}" pydir="${SP_DIR}" shared=yes -j1
+else
+  make prefix="${PREFIX}" pydir="${SP_DIR}" shared=yes -j${CPU_COUNT}
+fi
+make prefix="${PREFIX}" pydir="${SP_DIR}" shared=yes install install-shared-python
+
+if [[ "$target_platform" == osx* ]]; then
+    install_name_tool -id @rpath/libmupdfcpp.dylib $PREFIX/lib/libmupdfcpp.dylib
+fi
